08903e3005cfe8a60cc5bdab3001a3b9b4e58cd3
thye <thyevidal@gmail.com>
2023-10-23 15:02:26 -0300

    Porcetagem alteravel

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 document.getElementById('btn').addEventListener('click', function () {
     const planta = parseInt(document.getElementById('planta').value);
     const alcool = parseInt(document.getElementById('alcool').value);
+    const percentual = parseFloat(document.getElementById('percentual').value);
 
-    let p10 = planta*10;
-    let mac = ((p10-(p10*0.25))-alcool);
-    let fil = p10*0.25;
+    //colocar o percentual no formato decimal
+    let percent = percentual/100
+    //calcula o total de planta
+    let peso_planta_total = planta*10;
+    //calcula a quantidade percentual de planta
+    let porcetagem_planta = peso_planta_total*percent;
 
-    document.getElementById('maceracao').textContent = mac + ' ml'
-    document.getElementById('filtragem').textContent = fil + ' ml'
+    //calculo da maceraçao e filtragem
+    let maceracao = ((peso_planta_total-porcetagem_planta)-alcool);
+    let filtragem = porcetagem_planta;
+
+    document.getElementById('maceracao').textContent = maceracao + ' ml'
+    document.getElementById('filtragem').textContent = filtragem + ' ml'
     
 })
